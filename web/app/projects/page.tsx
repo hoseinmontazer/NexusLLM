45b4c60d5959579821ca43b26694868ce7a24b58
h.montazer <h.montazer@uid.ir>
2026-07-12 01:51:18 +0330

    add applicationset

--- web/app/projects/page.tsx
+++ web/app/projects/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { useSearchParams } from 'next/navigation'
 import Link from 'next/link'
